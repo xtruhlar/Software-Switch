faf3d71e171af7fd8a89139bcc50f0ff69f8c187
David Truhlar <114069715+xtruhlar@users.noreply.github.com>
2025-01-04 22:58:18 +0000

    win_apple_supported

--- pythonProject/switch/utilities.py
+++ pythonProject/switch/utilities.py
@@ -1,27 +1,47 @@
 import psutil
 from PyQt5 import QtGui
 import time
+import platform
 
 
 def get_interfaces():  # Get the interfaces
+    actual_system = platform.system()
+    if actual_system == 'Windows':
+        ports = get_interfaces_windows()
+        return ports
+    elif actual_system == 'Darwin':
+        ports = get_interfaces_mac()
+        return ports
+    else:
+        return 'Unsupported OS'
+
+def get_interfaces_mac():
     ports = {}
     for intf, mac_addr in psutil.net_if_addrs().items():  # psutil for system information
         for i in mac_addr:
             if i.family == psutil.AF_LINK:  # if the family is a link - Ethernet
                 ports[intf] = i.address  # add the interface and the mac address to the dictionary
 
-    #keys_to_remove = [x for x in ports if "Ethernet" not in x or "vEthernet" in x or "en0" in x or "en1" in x]
-    # -------------------------------------------------- debug -------------------------------------------------- #
-    # Explanation: If the interface is not Ethernet, I need to test with the Wi-Fi interface
-    # if 'Wi-Fi' in keys_to_remove:
-    #     keys_to_remove.remove('Wi-Fi')
-    #     keys_to_remove.remove('vEthernet (WSL (Hyper-V firewall))')
-    # -------------------------------------------------- debug -------------------------------------------------- #
-    #for key in keys_to_remove:  # remove the keys that are not Ethernet
-    #   del ports[key]
-
-    if 'lo0' not in ports:
+    if 'lo0' not in ports or len(ports) < 2:
         ports['lo0'] = '00:00:00:00:00:00'  # Loopback interfaces do not have a MAC address
+
+    return ports
+
+def get_interfaces_windows():
+    ports = {}
+    for intf, mac_addr in psutil.net_if_addrs().items():  # psutil for system information
+        for i in mac_addr:
+            if i.family == psutil.AF_LINK:  # if the family is a link - Ethernet
+                ports[intf] = i.address  # add the interface and the mac address to the dictionary
+
+    keys_to_remove = [x for x in ports if "Ethernet" not in x or "vEthernet" in x or "en0" in x or "en1" in x]
+
+    if 'Wi-Fi' in keys_to_remove:
+        keys_to_remove.remove('Wi-Fi')
+        keys_to_remove.remove('vEthernet (WSL (Hyper-V firewall))')
+
+    for key in keys_to_remove:  # remove the keys that are not Ethernet
+        del ports[key]
 
     return ports
 
